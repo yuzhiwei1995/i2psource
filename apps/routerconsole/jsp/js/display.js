f8a11da0a93b7e0a100a6a2ee8ae9d8d1be79b7a
YZW <2896019492@qq.com>
2020-11-20 19:49:05 +0800

    'display'

--- apps/routerconsole/jsp/js/display.js
+++ apps/routerconsole/jsp/js/display.js
@@ -1,21 +1,27 @@
 $(document).ready(function(){
     console.log("ready");
+    displayOrHide();
     $("input[name='pmode']").change(function(){
         console.log('hello');
-        var rt = $("input[type='radio']:checked").val();
-        console.log(rt);
-        if(rt === 'I2PBridge'){
-            $('#i2pbridge').show();
-            $('#https1').hide();
-            $('#https2').hide();
-        } else if(rt === ''){
-            $('#i2pbridge').hide();
-            $('#https1').hide();
-            $('#https2').hide();
-        } else {
-            $('#i2pbridge').hide();
-            $('#https1').show();
-            $('#https2').show();
-        }
+        displayOrHide();
     })
 })
+
+function displayOrHide() {
+    var rt = $("input[type='radio']:checked").val();
+    console.log(rt);
+    if(rt === 'I2PBridge'){
+        $('#i2pbridge').show();
+        $('#https1').hide();
+        $('#https2').hide();
+
+    } else if(rt === ''){
+        $('#i2pbridge').hide();
+        $('#https1').hide();
+        $('#https2').hide();
+    } else {
+        $('#i2pbridge').hide();
+        $('#https1').show();
+        $('#https2').show();
+    }
+}
