e8d33a1c2063bdc839c496b2fb849d4b49440129
YZW <2896019492@qq.com>
2020-11-20 19:26:15 +0800

    display

--- apps/routerconsole/jsp/js/display.js
+++ apps/routerconsole/jsp/js/display.js
@@ -1,18 +1,21 @@
-$('.optbox').onchange(function showAndHide() {
-
-    var rt = $("input[type='radio']:checked").val();
-    alert(rt)
-    if(rt === 'I2PBridge'){
-        $('#i2pbridge').style.display="";
-        $('#https1').style.display="none";
-        $('#https2').style.display="none";
-    } else if(rt === ''){
-        $('#i2pbridge').style.display="none";
-        $('#https1').style.display="none";
-        $('#https2').style.display="none";
-    } else {
-        $('#i2pbridge').style.display="none";
-        $('#https1').style.display="";
-        $('#https2').style.display="";
-    }
+$(document).ready(function(){
+    console.log("ready");
+    $("input[name='pmode']").change(function(){
+        console.log('hello');
+        var rt = $("input[type='radio']:checked").val();
+        console.log(rt);
+        if(rt === 'I2PBridge'){
+            $('#i2pbridge').show();
+            $('#https1').hide();
+            $('#https2').hide();
+        } else if(rt === ''){
+            $('#i2pbridge').hide();
+            $('#https1').hide();
+            $('#https2').hide();
+        } else {
+            $('#i2pbridge').hide();
+            $('#https1').show();
+            $('#https2').show();
+        }
+    })
 })
